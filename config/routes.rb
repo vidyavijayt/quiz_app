d376a5d6e842e66e347cec2538dac8fa030dbb43
Vidya Vijay <vidyav@qburst.com>
2016-05-17 16:32:20 +0530

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'welcome#index'
+  
   # Routes for APIs
   namespace :api do
     get 'questions' => 'question#questions'
